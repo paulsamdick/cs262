173cf55cf14faded4fb315c75a8df87a9989f1db
paulsamdick <90645514+paulsamdick@users.noreply.github.com>
2021-10-19 00:53:59 -0400

    Update home.js

--- Projects/monopolyProject/screens/home.js
+++ Projects/monopolyProject/screens/home.js
@@ -48,32 +48,3 @@ export default function HomeScreen ({navigation}) {
   );
 };
 
-/*
-export default function HomeScreen ({navigation}) {
-    const [reviews, setReviews] = useState ([
-        { title: "Fellowship of the Ring", rating: 8.8, key: '1',
-                description : "some hobbits begin a quest."},
-        { title: "Two Towers", rating: 8.7, key: '2',
-                description: 'Some dark lords try to take over the world.'},
-        { title: "Return of the King", rating: 8.9, key: '3',
-                description: 'The dark lords are defeated, to much rejoicing.'},
-    
-    ]);
-
-    return (
-      <View style={globalStyles.container}>
-            {/* Get rid of that ugly button and, instead, display our list of movies. */
-            /*
-            <FlatList data={reviews} renderItem={({ item })=> (
-                <TouchableOpacity onPress={() => navigation.navigate('Details', item)}>
-                    <Text style = {titleStyle.container}> { item.title } </Text>
-                </TouchableOpacity>
-            )} />
-            
-           <TouchableOpacity onPress={() => navigation.navigate('About') } style={buttonStyles.button}>
-				              <Text style={buttonStyles.buttonText}>About</Text>
-			</TouchableOpacity>
-        </View>
-    );
-}
-*/
